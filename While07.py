3133c9551310d4eeffbf1d39d6acff4a97fa8bdd
unknown <princariens@gmail.com>
2022-04-07 10:07:56 +0500

    07

--- While07.py
+++ While07.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    evens="02468"
+    count = 0
+    i=0
+    while i < len(s):
+        if s[i] in evens:
+            count += 1
+        i += 1
+    return count
