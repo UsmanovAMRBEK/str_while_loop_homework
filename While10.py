06d9ad8ad1247d795ed0fec0e58594d9b661381a
unknown <princariens@gmail.com>
2022-04-07 10:10:53 +0500

    10

--- While10.py
+++ While10.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    sum=0
+    i=0
+    while i < len(s):
+        if int(s[i])%2==1:
+            sum+=int(s[i])
+        i+=1
+    return sum
